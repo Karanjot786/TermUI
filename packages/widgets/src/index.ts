27fb21c0c59790885951ec60c12737b670bebdd4
Saamya Narayan <saamyanarayan07@gmail.com>
2026-06-02 02:07:49 +0530

    feature: add Panel widget with title in top border

--- packages/widgets/src/index.ts
+++ packages/widgets/src/index.ts
@@ -115,3 +115,7 @@ export { DirectoryTree } from './display/DirectoryTree.js';
 export type { DirectoryTreeOptions } from './display/DirectoryTree.js';
 export { Tooltip } from './display/Tooltip.js';
 export type { TooltipOptions } from './display/Tooltip.js';
+
+
+
+export { Panel } from './layout/Panel.js';
