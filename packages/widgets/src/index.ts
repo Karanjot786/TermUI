4dbb24019b25f9fa9302095c5af37e03f099e594
lakshaygujjar273-cys <lakshaygujjar273@gmail.com>
2026-06-02 22:36:09 +0530

    fix: properly export Masonry widget

--- packages/widgets/src/index.ts
+++ packages/widgets/src/index.ts
@@ -71,7 +71,8 @@ export { Center } from './layout/Center.js';
 export type { CenterOptions } from './layout/Center.js';
 export { Card } from './layout/Card.js';
 export type { CardOptions } from './layout/Card.js';
-// Masonry removed: module not present in this package
+export { Masonry } from './layout/Masonry.js';
+export type { MasonryOptions } from './layout/Masonry.js';
 export { Columns } from './layout/Columns.js';
 export type { ColumnsOptions } from './layout/Columns.js';
 
