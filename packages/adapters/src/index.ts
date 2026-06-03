ad9045887e14892a3de7846ffdf7993957e523f8
Kajal Pandey <kajalpanday854@gmail.com>
2026-06-03 18:37:01 +0530

    feat(adapters): add GitHub adapter with Octokit support

--- packages/adapters/src/index.ts
+++ packages/adapters/src/index.ts
@@ -1,4 +1,17 @@
-export { useCommander } from './commander/index.js' 
-export type { CommanderResult } from './commander/index.js' 
+export { useCommander } from './commander/index.js'
+export type { CommanderResult } from './commander/index.js'
 export { useConf } from './conf/index.js'
 export type { SetConfValue, UseConfResult } from './conf/index.js'
+export { useGitHub } from './github/index.js'
+export type {
+  GitHubNamespace,
+  UseGitHubResult,
+  ReposListParams,
+  Repo,
+  IssuesListParams,
+  Issue,
+  PrsListParams,
+  PullRequest,
+  ReleasesListParams,
+  Release,
+} from './github/index.js'
