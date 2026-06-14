e7b8aae52ada4e184d955971c2d5516f7bf2a51b
kotebhakti30 <kotebhakti@gmail.com>
2026-06-14 18:26:41 +0530

    perf(website): remove router tests and fix manualChunks config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,9 +4,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['vite'],
-        },
+        manualChunks: {},
       },
     },
   },
