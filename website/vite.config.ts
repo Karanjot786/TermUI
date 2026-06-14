7acdda92499a3c12cbb597ce8680986935ff4de9
kotebhakti30 <kotebhakti@gmail.com>
2026-06-14 18:35:41 +0530

    perf(website): remove router tests, fix vite config, update landing page

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      output: {
-        manualChunks: {},
-      },
+      output: {},
     },
   },
 });
