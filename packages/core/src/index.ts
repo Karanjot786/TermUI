9d30f750842ad95186ce7a0315fbc2e7c966c5d4
jainiksha <jainiksha.patel2007@gmail.com>
2026-06-17 13:39:14 +0530

    feat: add session management and restoration API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -93,3 +93,4 @@ export * as ansi from './utils/ansi.js';
 export { writeClipboard, readClipboard, clipboard } from './utils/ansi.js';
 export { debounce } from './utils/debounce.js';
 export type { DebounceOptions } from './utils/debounce.js';
+export * from './session/Session.js';
